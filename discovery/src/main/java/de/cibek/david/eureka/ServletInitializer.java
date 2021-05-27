2d5050092b611643c7868354ba558428da0b9fce
trippledave <trippledave@users.noreply.github.com>
2021-05-27 21:10:10 +0200

    b

---
+++ discovery/src/main/java/de/cibek/david/eureka/ServletInitializer.java
@@ -0,0 +1,12 @@
+package de.cibek.david.eureka;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(EurekaApplication.class);
+//	}
+//        
+}
